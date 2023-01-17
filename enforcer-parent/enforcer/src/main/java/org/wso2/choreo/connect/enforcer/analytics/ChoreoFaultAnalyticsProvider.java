4ac57f0c0c5034dd08ec4b96f0e9f8aa29c8a7bd
SarithRavI <imaduwagesarith932@gmail.com>
2023-01-17 17:41:13 +0000

    Revert "Restored the main branch to main of upstream."
    This reverts commit 801d8773b2036b088b44c84051a848f32868c192.

--- enforcer-parent/enforcer/src/main/java/org/wso2/choreo/connect/enforcer/analytics/ChoreoFaultAnalyticsProvider.java
+++ enforcer-parent/enforcer/src/main/java/org/wso2/choreo/connect/enforcer/analytics/ChoreoFaultAnalyticsProvider.java
@@ -237,10 +237,6 @@ public class ChoreoFaultAnalyticsProvider implements AnalyticsDataProvider {
         return null;
     }
 
-    public String getUserName() {
-        return null;
-    }
-
     @Override
     public Map<String, Object> getProperties() {
         AnalyticsCustomDataProvider customDataProvider = AnalyticsFilter.getAnalyticsCustomDataProvider();
